0b1afd314672158cc1327aa90d683ee9cc73484d
asharifah <231708W@mymail.nyp.edu.sg>
2025-07-02 17:40:42 +0800

    staff master

---
+++ SpotTheScam/Staff.Master.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SpotTheScam
+{
+    public partial class Staff : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set user name from session or database
+                // For now, using default value set in the markup
+                // You can uncomment below to set from session:
+                // if (Session["UserName"] != null)
+                // {
+                //     lblUserName.Text = Session["UserName"].ToString();
+                // }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            // Clear session
+            Session.Clear();
+            Session.Abandon();
+
+            // Redirect to login page
+            Response.Redirect("~/Login.aspx");
+        }
+    }
+}
--- SpotTheScam/WebForm1.aspx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SpotTheScam.WebForm1" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-</asp:Content>
