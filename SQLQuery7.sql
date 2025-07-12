daaa381d5bf97612429c2b24fc7a6ac3de4c695c
ngcm26 <ngcm2611@gmail.com>
2025-07-12 23:42:15 +0800

    remove queryy files

--- SQLQuery7.sql
+++
@@ -1,9 +0,0 @@
-﻿-- Check if the foreign key references are correct
-SELECT 
-    ws.SessionId,
-    ws.Title,
-    e.ExpertName,
-    ws.ExpertId
-FROM WebinarSessions ws
-LEFT JOIN Experts e ON ws.ExpertId = e.ExpertId
-WHERE ws.SessionId = 1
